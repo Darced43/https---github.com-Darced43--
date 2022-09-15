6d75a47cd9b1f4caae12e91fc182ffda1c422aec
lapihin.k <k19-92@mail.ru>
2022-09-15 12:19:48 +0300

    правки по замечаниям

--- index.js
+++ index.js
@@ -4,42 +4,45 @@ const weatherIcon = document.querySelector('.weather_icon');
 const weatherLike = document.querySelector('.weather_like');
 const searchCity = document.querySelector('#searchCity');
 let cityEnter = "Киров"
+const forecastMorning = document.querySelector('.forecast__morning');
+const forecastDay = document.querySelector('.forecast__day');
+const forecastEvnight = document.querySelector('.forecast__evnight');
 
-async function weather (){
+async function fetchWeatherInfo (){
     try{
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityEnter}&appid=8863aa32b8dc6777a27722522c3c0462&units=metric`);
         const data = await response.json();
         console.log(data)
-        weatherTemp.innerHTML = parseInt(data.main.temp) + "°";
-        weatherCity.innerHTML = data.name;
-        weatherIcon.innerHTML = data.weather[0]['description']
+
+        const forecast = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityEnter}&appid=8863aa32b8dc6777a27722522c3c0462&units=metric`);
+        const dataCast = await forecast.json();
+        console.log(dataCast);
+
+        innerUI (data, dataCast);
     }catch(err){    
-        alert("ошибочка");
+        console.log("ошибочка");
     };
 }
 
-weather ()
+fetchWeatherInfo ()
 
-searchCity.addEventListener('keydown', function(event){
+function innerUI (data, dataCast){
+    weatherTemp.innerHTML = parseInt(data.main.temp) + "°";
+    weatherCity.innerHTML = data.name;
+    weatherIcon.innerHTML = data.weather[0]?.['description'];
+
+    forecastMorning.innerHTML = parseInt(dataCast.list[0].main.temp)+ "°";
+    forecastDay.innerHTML = parseInt(dataCast.list[2].main.temp)+ "°";
+    forecastEvnight.innerHTML = parseInt(dataCast.list[4].main.temp)+ "°";
+}
+
+searchCity.addEventListener('keydown', event => {
     if(event.code == 'Enter'){
-        cityEnter = this.value
+        cityEnter = searchCity.value
         console.log(cityEnter)
-        this.value = ''
+        searchCity.value = ''
     }
-    async function weather (){
-        try{
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityEnter}&appid=8863aa32b8dc6777a27722522c3c0462&units=metric`);
-            const data = await response.json();
-            console.log(data)
-            weatherTemp.innerHTML = parseInt(data.main.temp) + "°";
-            weatherCity.innerHTML = data.name;
-            weatherIcon.innerHTML = data.weather[0]['description']
-        }catch(err){
-            alert("ошибочка");
-        };
-    }
-    
-    weather ()
-
+    fetchWeatherInfo ()
 })
+
 
